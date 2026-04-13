8cc10e4ec65820ccf922e814a93192ccc22e423d
Mateusz Bizoń <2137zst@gmail.com>
2026-04-13 11:33:03 +0200

    zadanie 2

--- README.md
+++ README.md
@@ -24,5 +24,30 @@ Zapisz raport do nowego pliku tekstowego (raport.txt).
 
 - Wprowadznone Zmiany: Dodałem try catch przy otwieraniu pliku. Dodałem mierzenie czasu działania aplikacji.
 
+## Zadanie1
+- Opis: Zadaniem jest stworzenie w aplikacji konsolowej symulatora prostej, turowej walki rodem z gier RPG. Gracz będzie kontrolował bohatera, który walczy z losowo wybranym przeciwnikiem.  
+Główne cele: 
+Stwórz abstrakcyjną klasę bazową Character, która będzie zawierać wspólne cechy dla wszystkich postaci w grze: 
+Właściwości: Name (string), HealthPoints (int), MaxHealthPoints (int), AttackPower (int). 
+Metody: TakeDamage(int damage) (obniża HealthPoints), IsAlive() (zwraca true, jeśli HealthPoints > 0). 
+Metodę abstrakcyjną: Attack(Character target). 
+
+Stwórz klasy dziedziczące po klasie Character: 
+Hero: Reprezentuje postać gracza. 
+Monster: Reprezentuje przeciwnika. Możesz stworzyć kilka różnych potworów, które również dziedziczą po Monster i mają różne statystyki. 
+
+Zaimplementuj logikę ataku: Każda klasa (Hero, Monster) musi implementować metodę Attack w unikalny sposób. Na przykład: 
+Atak Hero może mieć szansę na cios krytyczny (zadaje podwójne obrażenia). 
+Atak Goblin może być szybki, ale słaby. 
+Atak Orc może być powolny, ale bardzo silny. 
+
+Stwórz klasę BattleArena, która będzie zarządzać przebiegiem pojedynku. 
+W konstruktorze powinna przyjmować dwie postacie (Character player, Character opponent). 
+Powinna zawierać metodę StartBattle(), która w pętli while będzie kontynuować walkę, dopóki jedna z postaci nie zginie. W każdej turze gracz atakuje przeciwnika, a następnie przeciwnik atakuje gracza. 
+Po każdej akcji, na konsoli powinien pojawić się czytelny log, np. "Hero 'Arthur' attacks and deals 15 damage to the Goblin! The Goblin now has 35/50 HP." 
+
+Główna część programu (Main): Stwórz obiekt Hero i losowego Monster, a następnie przekażcie ich do BattleArena i rozpocznij pojedynek. 
+
+- Wprowadzone zmiany: 
 ## Autor
 Mateusz Bizoń
--- Zadanie 2/Zadanie 2/Program.cs
+++ Zadanie 2/Zadanie 2/Program.cs
@@ -1,15 +1,171 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Zadanie_2
+namespace RpgSimulator
 {
-    internal class Program
+    // 1. Abstrakcyjna klasa bazowa
+    public abstract class Character
+    {
+        public string Name { get; set; }
+        public int HealthPoints { get; set; }
+        public int MaxHealthPoints { get; set; }
+        public int AttackPower { get; set; }
+
+        protected Character(string name, int maxHealth, int attack)
+        {
+            Name = name;
+            MaxHealthPoints = maxHealth;
+            HealthPoints = maxHealth;
+            AttackPower = attack;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            HealthPoints -= damage;
+            if (HealthPoints < 0) HealthPoints = 0;
+        }
+
+        public bool IsAlive() => HealthPoints > 0;
+
+        public abstract void Attack(Character target);
+    }
+
+    // 2. Klasa Bohatera
+    public class Hero : Character
+    {
+        public Hero(string name) : base(name, 100, 20) { }
+
+        public override void Attack(Character target)
+        {
+            Random rnd = new Random();
+            int damage = AttackPower;
+
+            if (rnd.Next(1, 6) == 1) // 20% szansy na krytyka
+            {
+                damage *= 2;
+                Console.WriteLine($"[KRYTYK!] {Name} uderza z pełną mocą!");
+            }
+
+            Console.WriteLine($"{Name} atakuje i zadaje {damage} obrażeń postaci {target.Name}!");
+            target.TakeDamage(damage);
+        }
+    }
+
+    // 3. Baza dla potworów
+    public abstract class Monster : Character
+    {
+        protected Monster(string name, int hp, int attack) : base(name, hp, attack) { }
+    }
+
+    // 4. Konkretne potwory
+    public class Goblin : Monster
+    {
+        public Goblin() : base("Goblin Złodziej", 50, 8) { }
+
+        public override void Attack(Character target)
+        {
+            Console.WriteLine($"{Name} atakuje szybko i zadaje {AttackPower} obrażeń!");
+            target.TakeDamage(AttackPower);
+
+            if (new Random().Next(1, 3) == 1) // 50% szansy na drugi cios
+            {
+                Console.WriteLine($"[COMBO!] {Name} poprawia szybkim ciosem za {AttackPower / 2} obrażeń!");
+                target.TakeDamage(AttackPower / 2);
+            }
+        }
+    }
+
+    public class Orc : Monster
+    {
+        public Orc() : base("Wściekły Ork", 120, 25) { }
+
+        public override void Attack(Character target)
+        {
+            if (new Random().Next(1, 5) == 1) // 25% szansy na pudło
+            {
+                Console.WriteLine($"[PUDŁO!] {Name} zamachnął się potężnie, ale nie trafił!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} miażdży ciosem i zadaje {AttackPower} obrażeń!");
+                target.TakeDamage(AttackPower);
+            }
+        }
+    }
+
+    // 5. Arena Walki
+    public class BattleArena
+    {
+        private Character Player;
+        private Character Opponent;
+
+        public BattleArena(Character player, Character opponent)
+        {
+            Player = player;
+            Opponent = opponent;
+        }
+
+        public void StartBattle()
+        {
+            Console.WriteLine("\n=== ROZPOCZYNA SIĘ WALKA! ===");
+            Console.WriteLine($"{Player.Name} ({Player.HealthPoints} HP) VS {Opponent.Name} ({Opponent.HealthPoints} HP)\n");
+
+            int turn = 1;
+
+            // Pętla walki
+            while (Player.IsAlive() && Opponent.IsAlive())
+            {
+                Console.WriteLine($"--- TURA {turn} ---");
+
+                // Atak Gracza
+                Player.Attack(Opponent);
+                Console.WriteLine($"{Opponent.Name} ma teraz {Opponent.HealthPoints}/{Opponent.MaxHealthPoints} HP.\n");
+
+                // Sprawdzamy czy potwór zginął po naszym ataku
+                if (!Opponent.IsAlive()) break;
+
+                // Atak Przeciwnika
+                Opponent.Attack(Player);
+                Console.WriteLine($"{Player.Name} ma teraz {Player.HealthPoints}/{Player.MaxHealthPoints} HP.\n");
+
+                turn++;
+            }
+
+            // Podsumowanie bitwy
+            Console.WriteLine("=== KONIEC WALKI ===");
+            if (Player.IsAlive())
+                Console.WriteLine($"Zwycięża {Player.Name}! Gratulacje!");
+            else
+                Console.WriteLine($"Niestety, {Player.Name} poległ w walce... Wygrywa {Opponent.Name}.");
+        }
+    }
+
+    // 6. Główna część programu (Main)
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=== WITAJ W RPG BATTLE SIMULATOR ===");
+            Console.Write("Podaj imię swojego bohatera: ");
+            string name = Console.ReadLine();
+
+            // Tworzenie bohatera
+            Hero player = new Hero(string.IsNullOrWhiteSpace(name) ? "Arthur" : name);
+
+            // Losowanie potwora
+            Monster opponent;
+            if (new Random().Next(1, 3) == 1)
+                opponent = new Goblin();
+            else
+                opponent = new Orc();
+
+            Console.WriteLine($"Twoim przeciwnikiem jest {opponent.Name}!");
+
+            // Rozpoczęcie walki
+            BattleArena arena = new BattleArena(player, opponent);
+            arena.StartBattle();
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
         }
     }
 }
